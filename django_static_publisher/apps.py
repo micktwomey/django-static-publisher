7cefa9e7fb38512793ee9fbe34a662832a51b961
Michael Twomey <mick@twomeylee.name>
2023-08-21 23:06:18 +0100

    Add more TODOs

--- django_static_publisher/apps.py
+++ django_static_publisher/apps.py
@@ -9,6 +9,8 @@ class DjangoStaticPublisherConfig(AppConfig):
 
     def ready(self):
         """Populate static patterns"""
+        # TODO: Allow settings to point to more static_publish locations
+        # TODO: Check for mysite.static_publish (sibling to settings?)
         for app_config in apps.get_app_configs():
             # For each app try to import the static_patterns module
             # which will register them as a side effect
